1c5976d201c438a678839b3e7ec9610bb85df1a0
Pornpol Wasuwat <pornpol.w@gmail.com>
2022-01-05 06:57:46 +0000

    use bignumber.js

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const BigNumber = require('bignumber.js');
 
 BigNumber.set({ EXPONENTIAL_AT: 1e+9 }) // Almost never return exponential notation
 
-const key = crypto.randomBytes(32).toString('base64');
-console.log(`key: ${key}\n`);
-
 const cid1 = '1204565412398';
 const cid2 = '1204565412399';
 
+const key = crypto.randomBytes(32).toString('base64');
 const salt = crypto.randomBytes(32).toString('base64');
+console.log(`key: ${key}`);
 console.log(`salt: ${salt}\n`);
 
 // Generate base58 PIN from Key & Salt
@@ -30,11 +29,13 @@ const createShares = (secret, cid) => {
   let bigSecret = BigNumber(Buffer.from(secret, 'base64').toString('hex'), 16);
   let bigCid = BigNumber(cid).pow(6); // TODO: Make CID More Complex
 
+  // Equation
   const share = bigCid.minus(bigSecret).times(2).plus(bigSecret);
-
-  console.log(`bigSecret: ${bigSecret}, length: ${bigSecret.toString().length}`)
-  console.log(`bigCid: ${bigCid}`)
-  console.log(`bigShare: ${share.toString()}\n`)
+  console.log(`------------------------------- cid: ${cid} -----------------------------`)
+  console.log(`bigSecret (number): ${bigSecret}, length: ${bigSecret.toString().length}`)
+  console.log(`bigCid (number): ${bigCid}`)
+  console.log(`share (number): ${share.toString()}`)
+console.log(`--------------------------------------------------------------------------------`)
 
   return Buffer.from(share.toString()).toString('base64')
 }
@@ -44,6 +45,7 @@ const recoverSecret = (cid, share) => {
   let bigCid = BigNumber(cid).pow(6); // TODO: Make CID More Complex
   let bigShare = BigNumber(Buffer.from(share, 'base64').toString());
 
+  // Equation
   const secret = bigCid.times(2).minus(bigShare)
 
   return Buffer.from(secret.toString(16), 'hex').toString('base64');
@@ -51,8 +53,9 @@ const recoverSecret = (cid, share) => {
 
 ///////////////////////// Test //////////////////////////////
 const share1 = createShares(key, cid1);
+console.log(`share1: ${share1}\n`);
+
 const share2 = createShares(key, cid2);
-console.log(`share1: ${share1}`);
 console.log(`share2: ${share2}\n`);
 
 const recoverSecret1 = recoverSecret(cid1, share1);
